0992582475d7258805a6f1bac0d35c315b337305
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2024-09-19 19:05:19 +0200

    initial commit

---
+++ src/main/java/cloud/cholewa/ai/AiServiceApplication.java
@@ -0,0 +1,12 @@
+package cloud.cholewa.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiServiceApplication.class, args);
+    }
+}
